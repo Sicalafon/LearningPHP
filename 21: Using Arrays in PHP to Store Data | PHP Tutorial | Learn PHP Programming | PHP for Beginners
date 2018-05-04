45966e6f713dfd997d17219842dfda9b740c2b59
Bartek <tyralabartosz@gmail.com>
2018-05-04 16:04:16 +0200

    First commit

---
+++ 21: Using Arrays in PHP to Store Data | PHP Tutorial | Learn PHP Programming | PHP for Beginners
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP Loops</title>
+
+</head>
+
+  <body>
+
+    <?php
+
+    //Loop
+    //whileLoop
+    //Do While Loop
+    //For Loop
+    //For each Loop
+    $array = array('Daniel','Jane','Jacob');
+
+    foreach ($array as $loopdata) {
+      echo "My name is ".$loopdata."<br>";
+    }
+    ?>
+
+  </body>
+</html>
---
+++ What_Are_Internal_Functions_in_PHP.php
@@ -0,0 +1,27 @@
+//This one is for one-line comment
+/*
+
+ This one is for multiple line comment
+
+*/
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='UTF-8'>
+<title>Title of the document</title>
+</head>
+<body>
+<form method="GET">
+<input type="text" name="person">
+<button>SUBMIT</button>
+</form>
+<?php
+$name = "Daniel ";
+//echo $name." is a handsome fellow!";
+print 10+5;
+?>
+ 
+</body>
+</html>
+                                                  
