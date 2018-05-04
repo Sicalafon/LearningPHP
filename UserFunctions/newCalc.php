9a18830ba0b3539b8df5fa01838511dfe67bb546
Bartek <tyralabartosz@gmail.com>
2018-05-04 19:59:24 +0200

    3- Commit

---
+++ UserFunctions/newCalc.php
@@ -0,0 +1,9 @@
+<?php
+
+function newCalc($x){
+  $newnr = $x * 0.75;
+  echo "Here is 75% of what you wrote: ". $newnr;
+}
+
+
+ ?>
---
+++ header.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+<header>
+  <nav>
+    <ul>
+      <li><a href="">nav1</a></li>
+      <li><a href="">nav2</a></li>
+      <li><a href="">nav3</a></li>
+      <li><a href="">nav4</a></li>
+    </ul>
+  </nav>
+</header>
+</html>
