e0719fac02b7e02400b7636205e9cd33ffb0d4ea
Bartek <tyralabartosz@gmail.com>
2018-05-06 22:42:26 +0200

    6-commit

---
+++ 31: MySQL Insert Into Database | PHP Tutorial | Learn PHP Programming | PHP for Beginners.sql
@@ -0,0 +1,3 @@
+-- inserting a data into table
+
+insert into posts(subject,content,date) values ('This is the subject','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2018-12-14 16:38:01');
